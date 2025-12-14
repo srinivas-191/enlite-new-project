5758e6a35a038fdca9d592a262a5e17a2747db84
Rajoli Srinivas <rajolisrinivas2002@gmail.com>
2025-12-14 22:22:47 +0530

    url updated 4

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,6 +5,7 @@ import axios from "axios";
 // BASE URL
 // -----------------------------------------
 
+//latest
 export const API_BASE =
   import.meta.env.VITE_API_BASE ||
   "https://pleasant-nature-production-b708.up.railway.app/api/";
