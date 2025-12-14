5ac46d6ff809aa254fc513b2e083ff5de2f9ba49
Rajoli Srinivas <rajolisrinivas2002@gmail.com>
2025-12-14 22:13:21 +0530

    url updated 4

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,13 +5,13 @@ import axios from "axios";
 // BASE URL
 // -----------------------------------------
 
-// export const API_BASE =
-//   import.meta.env.VITE_API_BASE ||
-//   "https://pleasant-nature-production-b708.up.railway.app/api/";
-
 export const API_BASE =
   import.meta.env.VITE_API_BASE ||
-  "https://serene-love-production-5eb5.up.railway.app/api/";
+  "https://pleasant-nature-production-b708.up.railway.app/api/";
+
+// export const API_BASE =
+//   import.meta.env.VITE_API_BASE ||
+//   "https://serene-love-production-5eb5.up.railway.app/api/";
 
 
 // export const API_BASE =
